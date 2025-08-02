3d4c79015f11deebcb2b83f7a0288e8c7c8e1576
Sakshi Pandey <161960007+Sakshipandey04@users.noreply.github.com>
2025-08-02 21:16:30 +0530

    Create testbench.sv

---
+++ Day07-2x1Mux/behavioral_modeling/testbench.sv
@@ -0,0 +1,19 @@
+module tb_mux2x1_df;
+  reg A, B, Sel;
+  wire Y;
+  mux2x1_df uut(.A(A), .B(B), .Sel(Sel), .Y(Y));
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    A=0; B=0; Sel =0; #10;
+    A=0; B=0; Sel =1; #10;
+    A=0; B=1; Sel =0; #10;
+    A=0; B=1; Sel =1; #10;
+    A=1; B=0; Sel =0; #10;
+    A=1; B=0; Sel =1; #10;
+    A=1; B=1; Sel =0; #10;
+    A=1; B=1; Sel =1; #10;
+    $finish;
+  end
+endmodule
