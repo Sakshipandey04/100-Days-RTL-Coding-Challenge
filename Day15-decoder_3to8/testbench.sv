47b5d86b94e70111d50024c0d3188a64cf6e265d
Sakshi Pandey <161960007+Sakshipandey04@users.noreply.github.com>
2025-08-10 17:50:19 +0530

    Create testbench.sv

---
+++ Day15-decoder_3to8/testbench.sv
@@ -0,0 +1,28 @@
+`timescale 1ns/1ps
+module tb_decoder_3to8;
+  reg [2:0] in;
+  reg en;
+  wire [7:0] out;
+  
+  decoder_3to8 uut(
+    .in(in), .en(en), .out(out)
+  );
+  initial begin
+    $dumpfile("decoder_3to8.vcd");
+    $dumpvars(0, tb_decoder_3to8);
+    
+    en = 1;
+    in= 3'b000; #10;
+    in= 3'b001; #10;
+    in= 3'b010; #10;
+    in= 3'b011; #10;
+    in= 3'b100; #10;
+    in= 3'b101; #10;
+    in= 3'b110; #10;
+    in= 3'b111; #10;
+    
+    en= 0;
+    in= 3'b011; #10;
+    $finish;
+  end
+endmodule
