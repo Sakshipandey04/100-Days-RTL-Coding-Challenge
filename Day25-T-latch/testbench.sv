d533da02904a5d83c52c19c90022a9d3bed6734c
Sakshi Pandey <161960007+Sakshipandey04@users.noreply.github.com>
2025-08-22 19:19:48 +0530

    Update testbench.sv

--- Day25-T-latch/testbench.sv
+++ Day25-T-latch/testbench.sv
@@ -9,12 +9,15 @@ module tb_t_latch;
     clk=0;
     forever #5 clk= ~clk;
   end
+  initial begin
+    $monitor("Time=%0t | clk=%b | T=%b | Q=%b", $time, clk, T, Q);
+    
     $dumpfile("t_latch.vcd");
     $dumpvars(0, tb_t_latch);
     
-    T=0; #20;
+    T=0; #10;
     T=1; #20;
-    T=1; #20;
+    T=1; #30;
     T=0; #20;
     $finish;
   end
